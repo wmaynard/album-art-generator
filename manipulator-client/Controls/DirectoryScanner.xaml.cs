95da6f65e27d02ebef213a781d187f1cf29eb97c
Will Maynard <will@willmaynard.com>
2025-03-03 13:38:23 -0800

    Trying to add children without replacing panel contents...

---
+++ manipulator-client/Controls/DirectoryScanner.xaml.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maynard.ImageManipulator.Client.Controls;
+
+public partial class DirectoryScanner : ContentView
+{
+    public DirectoryScanner()
+    {
+        InitializeComponent();
+    }
+}
