1c54509be461f5c40e6774c047b88f2a2a65d5cc
Will Maynard <will@willmaynard.com>
2025-03-12 14:33:07 -0700

    Remove the original CLI project

--- image-manipulation/Models/Location.cs
+++
@@ -1,9 +0,0 @@
-namespace Maynard.Imaging.Models;
-
-public struct Location(string path)
-{
-    public string Directory = Path.GetDirectoryName(path);
-    public string Filename = Path.GetFileNameWithoutExtension(path);
-    public string Extension = Path.GetExtension(path);
-    public string OriginalPath = path;
-}
